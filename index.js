a2baff4936cec0b4c3fa42651af01dd48a0e0ecc
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-07-28 18:51:05 -0400

    added new styling using javascript

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Select some elements...
+
+let header = document.querySelector("#page-header")
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = "rotate(180deg)"
+}
+
+
+let dogNames = document.querySelector(".dog-name")
+dogNames.style.textAlign = "left"
+
+let footer = document.querySelector(".footer")
+footer.style.color ="#A02B2B"
+
